1eaff659a13cde30a701469e28a34fa21317792b
Kiwin <kikoro@live.dk>
2021-09-06 00:19:32 +0200

    add Color.ts

---
+++ src/ts/Color.ts
@@ -0,0 +1,15 @@
+interface Color
+{
+  getColorString(): string;
+}
+
+class Color
+{
+  constructor(colorValue: number)
+  {
+    this.colorValue = this.colorValue;
+  }
+  colorValue: number;
+}
+
+export default { Color }
